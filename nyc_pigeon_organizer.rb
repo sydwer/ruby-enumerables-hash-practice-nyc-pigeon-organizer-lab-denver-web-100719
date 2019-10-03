52dae58f4e5d2aa2882229b517b10cd8cac6f97c
Sydney Werbach <sydwer@gmail.com>
2019-10-03 08:17:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons_first = {}
+  data.each do |general_facts, branched_facts|
+    branched_facts.each do |detailed_fact, names_array|
+      names_array.each do |specific_name|
+      pigeons_first[specific_name] ||= {}
+      pigeons_first[specific_name][general_facts] ||= []
+      pigeons_first[specific_name][general_facts]<< detailed_fact.to_s  
+      end
+    end
+  end
+ return pigeons_first
 end
+
+
+#to_str vs to_s https://marcgg.com/blog/2017/01/23/ruby-to-s-to-str/

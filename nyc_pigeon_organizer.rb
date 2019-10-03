07983a3a4b4cdc229c0996b76a9f4126e7fc02b9
Sydney Werbach <sydwer@gmail.com>
2019-10-03 08:16:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,7 @@ def nyc_pigeon_organizer(data)
     branched_facts.each do |detailed_fact, names_array|
       names_array.each do |specific_name|
       pigeons_first[specific_name] ||= {}
-    if pigeons_first[specific_name][general_facts] == nil
-          pigeons_first[specific_name][general_facts] = []
-        end
+          pigeons_first[specific_name][general_facts] ||= []
         pigeons_first[specific_name][general_facts]<< detailed_fact.to_s  
       end
     end
